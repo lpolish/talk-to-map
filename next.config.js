c3ff0f1a156c72a5d6045e6ec64c4710c4887d48
Luis Pulido <remote@luispulido.com>
2025-04-26 10:26:00 -0700

    feat: update Next.js configuration with TypeScript and ESLint settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+}
 
-export default nextConfig; 
+export default nextConfig 
